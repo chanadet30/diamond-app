6085d30f92d1b4d6e89ed2e04c59b02429c52191
laurent <chanadet30@gmail.com>
2026-04-24 19:22:58 +0200

    fix lotus page

---
+++ app/lotus/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+export default function LotusPage() {
+  return (
+    <main style={{
+      background: "#000",
+      color: "white",
+      minHeight: "100vh",
+      padding: "80px",
+      textAlign: "center"
+    }}>
+      <h1>Page Lotus 💎</h1>
+      <p>Version test OK</p>
+    </main>
+  );
+}
